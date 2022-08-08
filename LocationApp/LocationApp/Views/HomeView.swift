b540ee01dfa33956ef4c09d97576c39e016e7dcf
Praneeth Vithanage <praneethvithanage10@gmail.com>
2022-08-08 17:10:40 +0530

    HomeView UI bug fix
    HomeView UI bug fix

--- LocationApp/LocationApp/Views/HomeView.swift
+++ LocationApp/LocationApp/Views/HomeView.swift
@@ -24,7 +24,7 @@ struct HomeView: View {
                     NavigationView {
                         Text("Searching \(searchText)")
                             .searchable(text: $searchText)
-                            .navigationTitle("Emploer Locations")
+                            .navigationTitle("Employer Locations")
                     }.frame(height: 180.0)
                     
                     if viewModel.userLists != nil && viewModel.postDetail != nil {
